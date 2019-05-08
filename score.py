e244240e8b6fcdf505285cdecb23efe4d14c8b90
Louie Lu <git@louie.lu>
2019-05-08 13:27:21 +0800

    Init repo

---
+++ score.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import namedtuple
+
+
+FlagPoints = namedtuple('FlagPoints', ['flag', 'points'])
+
+
+def get(f):
+    return map(lambda x: list(map(lambda x: x.strip(), x.strip().split(
+        ':'))), filter(lambda x: x.strip(), f.readlines()))
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('usage: score.py [input] [answer]')
+        exit(1)
+
+    input_fn = sys.argv[1]
+    answer_fn = sys.argv[2]
+
+    with open(input_fn) as f:
+        student_ids = next(f).strip().split(',')
+        input_lines = get(f)
+
+    with open(answer_fn) as f:
+        lines = get(f)
+        answer = {k: FlagPoints(v, int(s)) for k, v, s in lines}
+
+    score = 0
+    for k, v in input_lines:
+        if answer[k].flag == v:
+            score += answer[k].points
+
+    for student in student_ids:
+        print(f'{student}: {score}')
+
+
+if __name__ == '__main__':
+    main()
